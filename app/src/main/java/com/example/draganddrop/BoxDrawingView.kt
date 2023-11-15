cf0f0a8da56f537bf003765c315bc176a54bb99a
Jack Medrek <jackmedrek@Jacks-MacBook-Pro-2.local>
2023-11-15 11:07:55 -0500

    started in 11/15 lecture

---
+++ app/src/main/java/com/example/draganddrop/BoxDrawingView.kt
@@ -0,0 +1,54 @@
+package com.example.draganddrop
+
+import android.content.Context
+import android.graphics.PointF
+import android.util.AttributeSet
+import android.util.Log
+import android.view.MotionEvent
+import android.view.View
+
+private const val TAG = "BoxDrawingView"
+class BoxDrawingView(
+    context: Context,
+    attrs: AttributeSet? = null
+): View(context, attrs) {
+    private var currentBox: Box? = null
+    private val boxes = mutableListOf<Box>()
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        val current = PointF(event.x, event.y)
+        var action = ""
+
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                action = "ACTION_DOWN"
+                currentBox = Box(current).also {
+                    boxes.add(it)
+                }
+            }
+            MotionEvent.ACTION_MOVE -> {
+                action = "ACTION_MOVE"
+                updateCurrentBox(current)
+            }
+            MotionEvent.ACTION_UP -> {
+                action = "ACTION_UP"
+                updateCurrentBox(current)
+                currentBox = null
+            }
+            MotionEvent.ACTION_CANCEL -> {
+                action = "ACTION_CANCEL"
+                currentBox = null
+            }
+        }
+        Log.d(TAG, "$action at x=${current.x}, y=${current.y}")
+        return true
+    }
+
+    private fun updateCurrentBox(current: PointF) {
+        currentBox?.let {
+            it.end = current
+            invalidate()
+        }
+    }
+}
+
